7710abc3bdca5bc8b09c9bbc1d3a0b01b40f3d84
Ina Bryzheuskaya <inabri783@gmail.com>
2026-02-19 12:43:37 -0600

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ document.body.appendChild(footer);
 
 const today = new Date();
 
-// Current yera
+// Current year
 
 const thisYear = today.getFullYear();
 const copyright = document.createElement("p");
@@ -26,3 +26,39 @@ for (let i = 0; i < skills.length; i++) {
   skill.innerText = skills[i];
   skillsList.appendChild(skill);
 }
+
+// Leave a Message
+
+const messagesForm = document.querySelector("form[name='leave_message']");
+messagesForm.addEventListener('submit', function(event) {
+event.preventDefault();
+  const userName = event.target.userName.value;
+  const userEmail = event.target.userEmail.value;
+  const userMessage = event.target.userMessage.value;
+
+  console.log(userName, userEmail, userMessage);
+
+  const messageSection = document.querySelector("#Messages");
+  const messageList = messageSection.querySelector("ul");
+  const newMessage = document.createElement('li');
+  const userNameLink = document.createElement('a');
+  userNameLink.innerText = userName;
+  userNameLink.href = `mailto: ${userEmail}`;
+  const userMessageText = document.createElement('span');
+  userMessageText.innerText = userMessage;
+newMessage.appendChild(userNameLink);
+newMessage.appendChild(userMessageText);
+
+//new button
+
+const removeButton = document.createElement('button');
+removeButton.innerText = 'remove';
+removeButton.type = 'button';
+removeButton.addEventListener('click', function() {
+  const entry = removeButton.parentNode;
+  entry.remove();
+  })
+  newMessage.appendChild(removeButton);
+  messageList.appendChild(newMessage);
+  messagesForm.reset();
+})
